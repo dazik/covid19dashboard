2eb0aa16cbae7d16152905249fa8ccafe98639a2
Saidazizkhon Akbarov <akbaroff5@gmail.com>
2020-12-12 16:44:38 +0500

    refactor: loadCovidApi module removed, add fetch function and generating of two tables

--- src/index.js
+++ src/index.js
@@ -1,31 +1,85 @@
 import './styles/styles.scss';
-import data from './js/loadcoviddata';
-//let data;
 const url = 'https://api.covid19api.com/summary';
-const mainTable = document.querySelector('.table');
+const mainTableContainer = document.querySelector('#mainTable');
+const mapContainer = document.querySelector('#map');
+const countryTableContainer = document.querySelector('#countryTable');
+const chartContainer = document.querySelector('#chart')
+let data;
+let populationCoefficient;
 
+//Load data from API
+async function loadData(url) {
+    let response = await fetch(url);
+    if (response.ok) {
+        data = await response.json();
+        generateIndexPage(data);
+    } else {
+        alert('error');
+    }
+}
 
-//Generate table
+function generateIndexPage(data) {
+  //console.log(data);
+  calcPopulationCoefficient();
+  generateMainTable(data.Global);
+  generateCountryTable(data.Countries);
+}
+//Generate main table
 function generateMainTable(data) {
-    const tbody = document.createElement('tbody');
-    tbody.innerHTML = `<tr><th  scope="row">Total infected:</th><td>${data['TotalConfirmed']}</td></tr>
-    <tr><th  scope="row">Total deaths:</th><td>${data['TotalDeaths']}</td></tr>
-    <tr><th  scope="row">Total recovered:</th><td>${data['TotalRecovered']}</td></tr>
-    <tr><th  scope="row">New infected:</th><td>${data['NewConfirmed']}</td></tr>
-    <tr><th  scope="row">New deaths:</th><td>${data['NewDeaths']}</td></tr>
-    <tr><th  scope="row">New recovered:</th><td>${data['NewRecovered']}</td></tr>
-    <tr><th  scope="row">Total infected per 100 000:</th><td>${parseInt(data['TotalConfirmed']/100000)}</td></tr>
-    <tr><th  scope="row">Total deaths per 100 000:</th><td>${parseInt(data['TotalDeaths']/100000)}</td></tr>
-    <tr><th  scope="row">Total recovered per 100 000:</th><td>${parseInt(data['TotalRecovered']/100000)}</td></tr>
-    <tr><th  scope="row">New infected per 100 000:</th><td>${parseInt(data['NewConfirmed']/100000)}</td></tr>
-    <tr><th  scope="row">New deaths per 100 000:</th><td>${parseInt(data['NewDeaths']/100000)}</td></tr>
-    <tr><th  scope="row">New recovered per 100 000:</th><td>${parseInt(data['NewRecovered']/100000)}</td></tr>`
-    mainTable.appendChild(tbody);
+  const table = document.createElement('table')
+  const tbody = document.createElement('tbody');
+  tbody.innerHTML = `<tr><th  scope="row">Total infected:</th><td>${data['TotalConfirmed'].toLocaleString('en', { maximumFractionDigits: 0 })}</td></tr>
+  <tr><th  scope="row">Total deaths:</th><td>${data['TotalDeaths'].toLocaleString('en', { maximumFractionDigits: 0 })}</td></tr>
+  <tr><th  scope="row">Total recovered:</th><td>${data['TotalRecovered'].toLocaleString('en', { maximumFractionDigits: 0 })}</td></tr>
+  <tr><th  scope="row">New infected:</th><td>${data['NewConfirmed'].toLocaleString('en', { maximumFractionDigits: 0 })}</td></tr>
+  <tr><th  scope="row">New deaths:</th><td>${data['NewDeaths'].toLocaleString('en', { maximumFractionDigits: 0 })}</td></tr>
+  <tr><th  scope="row">New recovered:</th><td>${data['NewRecovered'].toLocaleString('en', { maximumFractionDigits: 0 })}</td></tr>
+  <tr><th  scope="row">Total infected per 100 000:</th><td>${parseInt(data['TotalConfirmed']/populationCoefficient)}</td></tr>
+  <tr><th  scope="row">Total deaths per 100 000:</th><td>${parseInt(data['TotalDeaths']/populationCoefficient)}</td></tr>
+  <tr><th  scope="row">Total recovered per 100 000:</th><td>${parseInt(data['TotalRecovered']/populationCoefficient)}</td></tr>
+  <tr><th  scope="row">New infected per 100 000:</th><td>${parseInt(data['NewConfirmed']/populationCoefficient)}</td></tr>
+  <tr><th  scope="row">New deaths per 100 000:</th><td>${parseInt(data['NewDeaths']/populationCoefficient)}</td></tr>
+  <tr><th  scope="row">New recovered per 100 000:</th><td>${parseInt(data['NewRecovered']/populationCoefficient)}</td></tr>`;
+  table.appendChild(tbody);
+  mainTableContainer.appendChild(table);
+}
+
+//Generate country table
+function generateCountryTable(data) {
+  const table = document.createElement('table')
+  const tbody = document.createElement('tbody');
+  console.log(data);
+  const sortedData = sortData(data, 'TotalConfirmed');
+  for(let i = 0; i < sortedData.length; i++) {
+    tbody.innerHTML += `<tr><th  scope="row">${sortedData[i][0]}</th><td>${sortedData[i][1].toLocaleString('en', { maximumFractionDigits: 0 })}</td></tr>`
+  }
+  table.appendChild(tbody);
+  countryTableContainer.appendChild(table);
+}
+
+//Sort by parameter function
+function sortData(data, parameter) {
+  let sortedData = [];
+  for (let i = 0; i < data.length; i++) {
+    sortedData.push([data[i].Country, data[i].[parameter]])
+  }
+  sortedData.sort(function(a,b) {
+    return b[1] - a[1];
+  })
+  console.log(sortedData);
+  return sortedData;
+}
+
+
+//Calculating the coefficient that shows cases per 100.000 people
+function calcPopulationCoefficient(population = 7700000000) {
+  populationCoefficient = population / 100000;
 }
 
 //Initialization function
-function init() {}
-
+function init() {
+  loadData(url);
+}
 //Initialize the app when all loaded
 window.addEventListener('DOMContentLoaded', function() {
     init();

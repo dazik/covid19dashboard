8c80ad62178b5212705eaf7c9989e6ac34e26a9e
Ibrokhim <ibrohimechanic@gmail.com>
2020-12-15 14:14:53 +0500

    feat: add some basic view

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+
+
+
 import './styles/styles.scss';
 console.log('it works!');
+
+let navItem = document.getElementsByClassName(".nav-item");
+
+// navItem.addEventListener("click", () => {
+//     console.log(navItem);
+// });
+

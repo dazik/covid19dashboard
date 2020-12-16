2be100fde4cd9ae08733e9901c093994032450fd
Ibrokhim <ibrohimechanic@gmail.com>
2020-12-16 16:04:17 +0500

    feat: add basic layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-
-
+// import script for bootstrap
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
 import './styles/styles.scss';
 console.log('it works!');
 
-let navItem = document.getElementsByClassName(".nav-item");
 
-// navItem.addEventListener("click", () => {
-//     console.log(navItem);
-// });
 

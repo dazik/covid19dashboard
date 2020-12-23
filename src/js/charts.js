be6035bc45244ee442b5e01d26c9318e75e1a75f
Saidazizkhon Akbarov <akbaroff5@gmail.com>
2020-12-24 01:42:27 +0500

    feat: add countries in chart

--- src/js/charts.js
+++ src/js/charts.js
@@ -1,26 +1,31 @@
-const url = 'https://disease.sh/v3/covid-19/historical/all?lastdays=all';
 const ctx = document.getElementById('myChart').getContext('2d');
 let ylabel = [];
 let xlabel = [];
 let data;
 
-async function getData() {
+async function getData(country) {
+	const url = `https://disease.sh/v3/covid-19/historical/${country}?lastdays=all`;
   const response = await fetch(url);
-  data = await response.json();
+	data = await response.json();
   return data;
 }
 
-async function sortData() {
-  await getData();
-  for (let i in data.cases) {
-    ylabel.push([data.cases[i]]);
-    xlabel.push(i);
-  }
-  console.log(ylabel);
-  console.log(xlabel);
+async function sortData(country) {
+  await getData(country);
+  if (country === 'all') {
+		for (let i in data.cases) {
+			ylabel.push([data.cases[i]]);
+			xlabel.push(i);
+		}
+	} else {
+		for (let i in data.timeline.cases) {
+			ylabel.push([data.timeline.cases[i]]);
+			xlabel.push(i);
+		}
+	}
 }
-async function drawChart() {
-  await sortData();
+async function drawChart(country = 'all') {
+  await sortData(country);
   const myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -56,7 +61,5 @@ async function drawChart() {
     },
   });
 }
-
-drawChart();
 
 export { drawChart };
